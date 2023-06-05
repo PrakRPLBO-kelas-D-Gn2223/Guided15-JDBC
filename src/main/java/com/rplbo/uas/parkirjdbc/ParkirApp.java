8285a48c099f1f68db90a53740522905c5047b81
Antonius Rachmat <antoniusrc@users.noreply.github.com>
2023-06-05 21:20:52 +0700

    Add files via upload

---
+++ src/main/java/com/rplbo/uas/parkirjdbc/ParkirApp.java
@@ -0,0 +1,28 @@
+package com.rplbo.uas.parkirjdbc;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class ParkirApp {
+    private String strcon;
+    private Connection conn;
+    private Scanner sc;
+    private Statement stmt;
+    
+    public void koneksi(){
+        
+    }
+
+    public void tambahData(){
+        
+    }
+
+    public void tampilData(){
+        
+
+    }
+
+    public void totalPemasukan(){
+        
+    }
+}
